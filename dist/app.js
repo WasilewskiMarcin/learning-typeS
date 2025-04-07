562b20b85f92016520baba869e44f9634fc8b6ec
Marcin Wasilewski <marcin.wasilewski0499@gmail.com>
2025-04-07 23:21:27 +0200

    first commit

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,6 @@
-console.log('test');
+let age = 29;
+age = 30;
+let ageAsString = 'dwa';
+ageAsString = `${age}`;
+const add = (v1, v2) => v1 + v2;
+console.log(add(10, 5));
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('test')
+let age: number = 29
+age = 30
+
+let ageAsString: string = 'dwa'
+ageAsString = `${age}`
+
+const add = (v1: number, v2: number) => v1 + v2
+
+console.log(add(10, 5))
