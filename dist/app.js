7ff573a95212542b9829cec3879acc85ebadb72e
WasilewskiMarcin <marcin.wasilewski0499@gmail.com>
2025-04-08 09:52:51 +0200

    boolean type

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,9 @@
-const input1Element = document.querySelector("#input1");
-const input2Element = document.querySelector("#input2");
-const addButtonElement = document.querySelector("#button");
-const add = (v1, v2) => v1 + v2;
-addButtonElement.addEventListener("click", () => {
-    const sum = add(Number(input1Element.value), Number(input2Element.value));
-    console.log(sum);
+const buttonElement = document.getElementById('button');
+const calculatePrice = (originalPrice, hasDiscount) => {
+    return hasDiscount ? originalPrice * 0.8 : originalPrice;
+};
+buttonElement.addEventListener('click', () => {
+    const originalPrice = 50;
+    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
+    console.log(calculatePrice(originalPrice, hasDiscount));
 });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-const input1Element: HTMLInputElement = document.querySelector("#input1");
-const input2Element: HTMLInputElement = document.querySelector("#input2");
-const addButtonElement = document.querySelector("#button");
 
-const add = (v1, v2) => v1 + v2;
+const buttonElement = document.getElementById('button');
 
-addButtonElement.addEventListener("click", () => {
-    const sum = add(Number(input1Element.value), Number(    input2Element.value));
-    console.log(sum);
+const calculatePrice = (originalPrice: number, hasDiscount: boolean)=>{
+    return hasDiscount ? originalPrice * 0.8 : originalPrice;
+}
+
+buttonElement.addEventListener('click', () => {
+    const originalPrice:number = 50;
+    const hasDiscount: boolean = new URLSearchParams(window.location.search).get("discount") === "true";
+    console.log(calculatePrice(originalPrice, hasDiscount));
 });
